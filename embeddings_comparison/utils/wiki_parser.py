97520221de1824ad366864f4ad50ad45eb8063c6
Andrey Pigarev <pigarev.a.g2014@gmail.com>
2024-11-12 21:08:20 +0100

    Removed references to other pages from text. Added support of parsing from file by file path

--- embeddings_comparison/utils/wiki_parser.py
+++ embeddings_comparison/utils/wiki_parser.py
@@ -11,11 +11,12 @@ def cleanText(text: str) -> str:
     
     text = re.sub(r"\[\[Plik:.*?\]\]", "", text)  # remove file references from text
     text = re.sub(r"\[\[(.*)\]\]", r"\1", text)  # remove square brackets surrounding references in text
+    text = re.sub(r"\{\{.*?\}\}", "", text) # remove references to other pages (usually in form of {{reference to other page}})
     text = re.sub(r" +", " ", text) # replace multiple spaces with single space
 
     return text
 
-def extractTexts(wiki_xml: str) -> list[SingleArticle]:
+def extractPagesFromString(wiki_xml: str) -> list[SingleArticle]:
     soup = BeautifulSoup(wiki_xml, "xml")
     
     pages = soup.findChildren('page')
@@ -26,3 +27,8 @@ def extractTexts(wiki_xml: str) -> list[SingleArticle]:
         text = cleanText(text)
         articles.append(SingleArticle(title=title, text=text))
     return articles
+
+def extractPagesFromFile(file_path: str) -> list[SingleArticle]:
+    with open(file_path, "r") as file:
+        wiki_xml = file.read()
+    return extractPagesFromString(wiki_xml)
--- tests/utils/test_wiki_parser.py
+++ tests/utils/test_wiki_parser.py
@@ -1,3 +1,4 @@
+from tempfile import NamedTemporaryFile
 import unittest
 
 from embeddings_comparison.utils import wiki_parser
@@ -22,7 +23,7 @@ class TestWikiParser(unittest.TestCase):
         wiki_articles_xml = self.contruct_wiki_xml([wiki_parser.SingleArticle(title="Test", text="Test text"), 
                                                     wiki_parser.SingleArticle(title="Test2", text="Test text 2")])
 
-        articles = wiki_parser.extractTexts(wiki_articles_xml)
+        articles = wiki_parser.extractPagesFromString(wiki_articles_xml)
 
         self.assertEqual(len(articles), 2)
         self.assertEqual(articles[0].title, "Test")
@@ -33,7 +34,7 @@ class TestWikiParser(unittest.TestCase):
     def testRemoveFileReferences(self):
         wiki_articles_xml = self.contruct_wiki_xml([wiki_parser.SingleArticle(title="Test", text="Test [[Plik:test text]] text")])
 
-        articles = wiki_parser.extractTexts(wiki_articles_xml)
+        articles = wiki_parser.extractPagesFromString(wiki_articles_xml)
 
         self.assertEqual(len(articles), 1)
         self.assertEqual(articles[0].title, "Test")
@@ -42,8 +43,32 @@ class TestWikiParser(unittest.TestCase):
     def testReplaceTextReferencesWithText(self):
         wiki_articles_xml = self.contruct_wiki_xml([wiki_parser.SingleArticle(title="Test", text="Test [[other text]] text")])
 
-        articles = wiki_parser.extractTexts(wiki_articles_xml)
+        articles = wiki_parser.extractPagesFromString(wiki_articles_xml)
 
         self.assertEqual(len(articles), 1)
         self.assertEqual(articles[0].title, "Test")
         self.assertEqual(articles[0].text, "Test other text text")
+
+    def testExtractingPagesFromFile(self):
+        wiki_articles_xml = self.contruct_wiki_xml([wiki_parser.SingleArticle(title="Test", text="Test text"), 
+                                                    wiki_parser.SingleArticle(title="Test2", text="Test text 2")])
+
+        with NamedTemporaryFile() as file:
+            file.write(wiki_articles_xml.encode('utf-8'))
+            file.seek(0)
+            articles = wiki_parser.extractPagesFromFile(file.name)
+
+            self.assertEqual(len(articles), 2)
+            self.assertEqual(articles[0].title, "Test")
+            self.assertEqual(articles[0].text, "Test text")
+            self.assertEqual(articles[1].title, "Test2")
+            self.assertEqual(articles[1].text, "Test text 2")
+
+    def testRemoveReferencesToOtherPages(self):
+        wiki_articles_xml = self.contruct_wiki_xml([wiki_parser.SingleArticle(title="Test", text="Test {{other page}} text")])
+
+        articles = wiki_parser.extractPagesFromString(wiki_articles_xml)
+
+        self.assertEqual(len(articles), 1)
+        self.assertEqual(articles[0].title, "Test")
+        self.assertEqual(articles[0].text, "Test text")
