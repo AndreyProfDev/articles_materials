c80c2da8f039fc350d2a30e976e2f1610ebd1f48
Andrey Pigarev <pigarev.a.g2014@gmail.com>
2024-11-13 22:37:21 +0100

    Extended wiki parser to remove different extra elements from wiki pages

--- embeddings_comparison/utils/wiki_parser.py
+++ embeddings_comparison/utils/wiki_parser.py
@@ -9,12 +9,26 @@ class SingleArticle(BaseModel):
 
 def cleanText(text: str) -> str:
     
-    text = re.sub(r"\[\[Plik:.*?\]\]", "", text)  # remove file references from text
-    text = re.sub(r"\[\[(.*)\]\]", r"\1", text)  # remove square brackets surrounding references in text
-    text = re.sub(r"\{\{.*?\}\}", "", text) # remove references to other pages (usually in form of {{reference to other page}})
-    text = re.sub(r" +", " ", text) # replace multiple spaces with single space
+    while True:
+        new_text = re.sub(r"\[\[Plik:[^\[][^\[]*?\]\]", "", text, flags=re.S)  # remove file references from text
+        new_text = re.sub(r"\[\[Kategoria:[^\[][^\[]*?\]\]", "", new_text, flags=re.S)  # remove category references from text
+        new_text = re.sub(r"\[\[[^\[][^\[]*\|([^\[][^\[]*?)\]\]", r"\1", new_text, flags=re.S)  # remove square brackets surrounding references in text
+        new_text = re.sub(r"\[\[([^\[][^\[]*?)\]\]", r"\1", new_text, flags=re.S)  # remove square brackets surrounding references in text
+        new_text = re.sub(r"\{\{.*?\}\}", "", new_text, flags=re.S) # remove references to other pages (usually in form of {{reference to \nother page}})
+        new_text = re.sub(r"<ref>.*?</ref>", "", new_text, flags=re.S) # remove references to other pages
+        new_text = re.sub(r"\{\|[^\{][^\|]*?\|\}", "", new_text, flags=re.S) # remove tables
+        new_text = re.sub(r" +", " ", new_text) # replace multiple spaces with single space
+        new_text = re.sub(r"\n+", "\n", new_text) # replace multiple new line symbols with single new line symbol
+        new_text = re.sub(r"\n\*", "\n", new_text, flags=re.S) # remove bullet points
+        new_text = re.sub(r"\'\'\'", "", new_text) # remove bold text markdown
+        new_text = re.sub(r"\'\'", "", new_text) # remove italic text markdown
 
-    return text
+        if new_text == text:
+            text = new_text
+            break
+        text = new_text
+
+    return text.strip()
 
 def extractPagesFromString(wiki_xml: str) -> list[SingleArticle]:
     soup = BeautifulSoup(wiki_xml, "xml")
@@ -23,9 +37,11 @@ def extractPagesFromString(wiki_xml: str) -> list[SingleArticle]:
     articles = []
     for page in pages:
         title = page.title.text.strip()
-        text = page.revision.text.strip()
+        text = page.revision.findChild('text').text.strip()
         text = cleanText(text)
-        articles.append(SingleArticle(title=title, text=text))
+        
+        if len(text) > 0:
+            articles.append(SingleArticle(title=title, text=text))
     return articles
 
 def extractPagesFromFile(file_path: str) -> list[SingleArticle]:
--- tests/utils/test_wiki_parser.py
+++ tests/utils/test_wiki_parser.py
@@ -13,7 +13,8 @@ class TestWikiParser(unittest.TestCase):
             page_text += f"""<page>
                                 <title>{article.title}</title>
                                 <revision>
-                                    <text>{article.text}</text>
+                                    <id>53701990</id>
+                                    <text bytes="7843">{article.text}</text>
                                 </revision>
                             </page>"""
             
@@ -31,8 +32,8 @@ class TestWikiParser(unittest.TestCase):
         self.assertEqual(articles[1].title, "Test2")
         self.assertEqual(articles[1].text, "Test text 2")
 
-    def testRemoveFileReferences(self):
-        wiki_articles_xml = self.contruct_wiki_xml([wiki_parser.SingleArticle(title="Test", text="Test [[Plik:test text]] text")])
+    def testRemoveFileReference(self):
+        wiki_articles_xml = self.contruct_wiki_xml([wiki_parser.SingleArticle(title="Test", text="Test [[Plik:test\ntext]] text")])
 
         articles = wiki_parser.extractPagesFromString(wiki_articles_xml)
 
@@ -40,14 +41,41 @@ class TestWikiParser(unittest.TestCase):
         self.assertEqual(articles[0].title, "Test")
         self.assertEqual(articles[0].text, "Test text")
 
-    def testReplaceTextReferencesWithText(self):
-        wiki_articles_xml = self.contruct_wiki_xml([wiki_parser.SingleArticle(title="Test", text="Test [[other text]] text")])
+    def testRemoveSeveralFileReferences(self):
+        wiki_articles_xml = self.contruct_wiki_xml([wiki_parser.SingleArticle(title="Test", text="Test [[Plik:test\ntext]]\n[[Plik:test\ntext]] text")])
 
         articles = wiki_parser.extractPagesFromString(wiki_articles_xml)
 
         self.assertEqual(len(articles), 1)
         self.assertEqual(articles[0].title, "Test")
-        self.assertEqual(articles[0].text, "Test other text text")
+        self.assertEqual(articles[0].text, "Test \n text")
+
+    def testReplaceSeveralTextReferencesWithText(self):
+        wiki_articles_xml = self.contruct_wiki_xml([wiki_parser.SingleArticle(title="Test", text="Test [[Plik:test\ntext]]\n[[other\ntext]] text")])
+
+        articles = wiki_parser.extractPagesFromString(wiki_articles_xml)
+
+        self.assertEqual(len(articles), 1)
+        self.assertEqual(articles[0].title, "Test")
+        self.assertEqual(articles[0].text, "Test \nother\ntext text")
+
+    def testReplaceTextReferenceWithTextInside(self):
+        wiki_articles_xml = self.contruct_wiki_xml([wiki_parser.SingleArticle(title="Test", text="Test [[other\ntext]] text")])
+
+        articles = wiki_parser.extractPagesFromString(wiki_articles_xml)
+
+        self.assertEqual(len(articles), 1)
+        self.assertEqual(articles[0].title, "Test")
+        self.assertEqual(articles[0].text, "Test other\ntext text")
+
+    def testReplaceTextReferenceWithProperText(self):
+        wiki_articles_xml = self.contruct_wiki_xml([wiki_parser.SingleArticle(title="Test", text="Test [[other|actualtext]] text")])
+
+        articles = wiki_parser.extractPagesFromString(wiki_articles_xml)
+
+        self.assertEqual(len(articles), 1)
+        self.assertEqual(articles[0].title, "Test")
+        self.assertEqual(articles[0].text, "Test actualtext text")
 
     def testExtractingPagesFromFile(self):
         wiki_articles_xml = self.contruct_wiki_xml([wiki_parser.SingleArticle(title="Test", text="Test text"), 
@@ -65,7 +93,115 @@ class TestWikiParser(unittest.TestCase):
             self.assertEqual(articles[1].text, "Test text 2")
 
     def testRemoveReferencesToOtherPages(self):
-        wiki_articles_xml = self.contruct_wiki_xml([wiki_parser.SingleArticle(title="Test", text="Test {{other page}} text")])
+        wiki_articles_xml = self.contruct_wiki_xml([wiki_parser.SingleArticle(title="Test", text=r"Test {{other page}} text")])
+
+        articles = wiki_parser.extractPagesFromString(wiki_articles_xml)
+
+        self.assertEqual(len(articles), 1)
+        self.assertEqual(articles[0].title, "Test")
+        self.assertEqual(articles[0].text, "Test text")
+
+    def testRemoveReferencesToOtherPagesMultiline(self):
+        wiki_articles_xml = self.contruct_wiki_xml([wiki_parser.SingleArticle(title="Test", text="Test {{other\npage\n}} text")])
+
+        articles = wiki_parser.extractPagesFromString(wiki_articles_xml)
+
+        self.assertEqual(len(articles), 1)
+        self.assertEqual(articles[0].title, "Test")
+        self.assertEqual(articles[0].text, "Test text")
+
+    def testRemoveReferencesToOtherPagesMultilineInTheBeginning(self):
+        wiki_articles_xml = self.contruct_wiki_xml([wiki_parser.SingleArticle(title="Test", text="{{other\npage\n}}\n\nTest text")])
+
+        articles = wiki_parser.extractPagesFromString(wiki_articles_xml)
+
+        self.assertEqual(len(articles), 1)
+        self.assertEqual(articles[0].title, "Test")
+        self.assertEqual(articles[0].text, "Test text")
+
+    def testFinalTextCleaning(self):
+        wiki_articles_xml = self.contruct_wiki_xml([wiki_parser.SingleArticle(title="Test", text="\n\nTest     text  ")])
+
+        articles = wiki_parser.extractPagesFromString(wiki_articles_xml)
+
+        self.assertEqual(len(articles), 1)
+        self.assertEqual(articles[0].title, "Test")
+        self.assertEqual(articles[0].text, "Test text")
+
+    def testRemovalOfMultipleNestedReferencesWithFileReferenceOnTopFromText(self):
+        wiki_articles_xml = self.contruct_wiki_xml([wiki_parser.SingleArticle(title="Test", text="Test [[Plik:Test reference\n[[Inner reference]] remaining part of the reference]] text")])
+
+        articles = wiki_parser.extractPagesFromString(wiki_articles_xml)
+
+        self.assertEqual(len(articles), 1)
+        self.assertEqual(articles[0].title, "Test")
+        self.assertEqual(articles[0].text, "Test text")
+
+    def testRemovalOfMultipleNestedReferencesWithFileReferenceOnTopFromText2(self):
+        wiki_articles_xml = self.contruct_wiki_xml([wiki_parser.SingleArticle(title="Test", text="Test [[Plik:Test reference\n[[Inner reference]]]] text")])
+
+        articles = wiki_parser.extractPagesFromString(wiki_articles_xml)
+
+        self.assertEqual(len(articles), 1)
+        self.assertEqual(articles[0].title, "Test")
+        self.assertEqual(articles[0].text, "Test text")
+
+    def testRemovalOfHTMLReferences(self):
+        wiki_articles_xml = self.contruct_wiki_xml([wiki_parser.SingleArticle(title="Test", text="Test <ref>other reference</ref> text")])
+
+        articles = wiki_parser.extractPagesFromString(wiki_articles_xml)
+
+        self.assertEqual(len(articles), 1)
+        self.assertEqual(articles[0].title, "Test")
+        self.assertEqual(articles[0].text, "Test other reference text")
+
+    def testRemovalOfCategories(self):
+        wiki_articles_xml = self.contruct_wiki_xml([wiki_parser.SingleArticle(title="Test", text="Test [[Kategoria:other category]] text")])
+
+        articles = wiki_parser.extractPagesFromString(wiki_articles_xml)
+
+        self.assertEqual(len(articles), 1)
+        self.assertEqual(articles[0].title, "Test")
+        self.assertEqual(articles[0].text, "Test text")
+
+    def testRemovalOfListMarkdown(self):
+        wiki_articles_xml = self.contruct_wiki_xml([wiki_parser.SingleArticle(title="Test", text="Test \n* list\n* list text")])
+
+        articles = wiki_parser.extractPagesFromString(wiki_articles_xml)
+
+        self.assertEqual(len(articles), 1)
+        self.assertEqual(articles[0].title, "Test")
+        self.assertEqual(articles[0].text, "Test \n list\n list text")
+
+    def testRemovalOfEmptyPages(self):
+        wiki_articles_xml = self.contruct_wiki_xml([wiki_parser.SingleArticle(title="Test", text=""), wiki_parser.SingleArticle(title="Test2", text="Test text 2")])
+
+        articles = wiki_parser.extractPagesFromString(wiki_articles_xml)
+
+        self.assertEqual(len(articles), 1)
+        self.assertEqual(articles[0].title, "Test2")
+        self.assertEqual(articles[0].text, "Test text 2")
+
+    def testRemovalBoldTextMarkdown(self):
+        wiki_articles_xml = self.contruct_wiki_xml([wiki_parser.SingleArticle(title="Test", text="Test '''bold''' text")])
+
+        articles = wiki_parser.extractPagesFromString(wiki_articles_xml)
+
+        self.assertEqual(len(articles), 1)
+        self.assertEqual(articles[0].title, "Test")
+        self.assertEqual(articles[0].text, "Test bold text")
+
+    def testRemovalOfItalicTextMarkdown(self):
+        wiki_articles_xml = self.contruct_wiki_xml([wiki_parser.SingleArticle(title="Test", text="Test ''italic'' text")])
+
+        articles = wiki_parser.extractPagesFromString(wiki_articles_xml)
+
+        self.assertEqual(len(articles), 1)
+        self.assertEqual(articles[0].title, "Test")
+        self.assertEqual(articles[0].text, "Test italic text")
+
+    def testRemovalOfTablesMarkdown(self):
+        wiki_articles_xml = self.contruct_wiki_xml([wiki_parser.SingleArticle(title="Test", text="Test {|table beginning\n{|table ending|} remaining part|} text")])
 
         articles = wiki_parser.extractPagesFromString(wiki_articles_xml)
 
